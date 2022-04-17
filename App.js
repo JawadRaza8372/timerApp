b706245cf91e3f4e52ee270861274104100c8cd9
Jawad Raza <jawadraza8372@gmail.com>
2022-04-17 10:30:59 +0500

    fializing

--- App.js
+++ App.js
@@ -3,44 +3,17 @@ import MyStackNavig from "./src/AppNavigation/MyStackNavig";
 import ActivityScreen from "./src/Views/ActivityScreen";
 import AuthScreen from "./src/Views/AuthScreen";
 import TimerScreen from "./src/Views/TimerScreen";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
+import { setUserActivity } from "./src/store/projectSlice";
 import { store } from "./src/store";
+import { db } from "./src/DataBase/Configer";
 export default function App() {
   LogBox.ignoreLogs([
     "Setting a timer",
     "Can't perform a React state update on an unmounted component",
     "AsyncStorage has been extracted from react-native",
   ]);
-  // db.collection("loans").onSnapshot((snapshot) => {
-  //   setCount(snapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() })));
-  // });
-  // await db
-  //   .collection("users")
-  //   .doc(`${myDoc?.id}`)
-  //   .update({
-  //     ...myDoc?.data,
-  //     password: newPassword,
-  //   })
-  //   .then(() => {
-  //     AlertFunction("Updated Successfully", "Password has been updated");
-  //   });
-  //  await db
-  //   .collection("authSystem")
-  //   .where("email", "==", `${loginInfo?.email}`)
-  //   .get()
-  //   .then((querySnapshot) => {
-  //     querySnapshot.forEach((doc) => {
-  //       if (doc.data().password === loginInfo?.password) {
-  //  // doc.data();
 
-  //
-  //       } else {
-  //       }
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error getting documents: ", error);
-  //   });
   return (
     <Provider store={store}>
       <>
